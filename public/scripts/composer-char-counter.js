8d253c98d9f91bbd60850c456860c491461bd289
Michael-Xie <xiemicha@gmail.com>
2019-12-19 14:59:36 +0000

    Cleaned up commented code for composer-char-counter

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,22 +1,16 @@
-$(document).ready(function() {
+$(document).ready(function () {
   // --- our code goes here ---
   console.log("hello");
 
-  $(".new-tweet textarea").on("input", function(event){
+  $(".new-tweet textarea").on("input", function (event) {
     const maxChar = 140;
     let currLen = $(this).val().length;
 
     let charLeft = maxChar - currLen;
-    console.log(currLen);
     $(".new-tweet span").toggleClass('error', charLeft < 0)
-    // if (charLeft < 0) {
-    //   $(".new-tweet span").css("color", "red");
-    // } else {
-    //   $(".new-tweet span").css("color", "black");
-    // }
 
     $(".new-tweet span").text(charLeft);
-    })
+  })
 
 });
 
